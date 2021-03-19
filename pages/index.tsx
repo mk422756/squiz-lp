df0bdae1e2b86312f0cbe9eec8035351336c8ef3
mk422756 <k422756+git@gmail.com>
2021-03-19 20:28:42 +0900

    favicon追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,28 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>SQUIZ</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#63B9ED" />
+        <meta name="theme-color" content="#63B9ED"></meta>
       </Head>
       <Layout>
         <Header></Header>
