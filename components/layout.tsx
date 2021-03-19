67bbb1788240860c06e3bf87494a13d405293b47
mk422756 <k422756+git@gmail.com>
2021-03-19 19:59:48 +0900

    調整

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({children}) {
   return (
     <div className="bg-blue_1">
-      <div className="max-w-xl mx-auto bg-white shadow">{children}</div>
+      <div className="max-w-lg mx-auto bg-white shadow">{children}</div>
     </div>
   )
 }
